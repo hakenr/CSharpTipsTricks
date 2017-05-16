adc042642e53a187cf551fcb4413fd753f6c80a2
Robert Haken <haken@havit.cz>
2017-05-17 00:59:48 +0200

    UsingWithoutVariable

---
+++ CSharpTipsTricks/UsingWithoutVariable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Demos.CSharpTipsTricks
+{
+	public class UsingWithoutVariable
+	{
+		public object GetList()
+		{
+			using (var uow = this.UnitOfWorkProvider.Create())
+			{
+				return this.QueryFactory.Execute();
+			}
+		}
+
+		public object GetList2()
+		{
+			using (this.UnitOfWorkProvider.Create())
+			{
+				return this.QueryFactory.Execute();
+			}
+		}
+
+		// see also
+		// Razor:	@using (Html.BeginForm(...)) { ... }
+		// ORM:		using (new IdentityMapScope()) { ... }
+
+
+
+
+
+		// fakes for illustration
+		public dynamic QueryFactory { get; }
+		public dynamic UnitOfWorkProvider { get; }
+
+	}
+}
