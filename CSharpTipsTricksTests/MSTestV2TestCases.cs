206cade104c3aaa6cd977db817bc65cb0333422b
Robert Haken <haken@havit.cz>
2017-05-16 19:15:22 +0200

    MSTestV2TestCases

---
+++ CSharpTipsTricksTests/MSTestV2TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Demos.CSharpTipsTricksTests
+{
+	[TestClass]
+	public class MSTestV2TestCases
+	{
+		[DataTestMethod]
+		[DataRow(1, 2, "3")]
+		[DataRow(10, 2, "12")]
+		[DataRow(10, 20, "13")]
+		public void MSTestV2TestCases_Add(int value1, int value2, string expected)
+		{
+			string Add(int op1, int op2) => (op1 + op2).ToString(); // C# 7.0 Local function
+
+			Assert.AreEqual(expected, Add(value1, value2));
+		}
+	}
+}
---
+++ CSharpTipsTricksTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpTipsTricksTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpTipsTricksTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a79cebc-b84a-441f-bd5b-20dda7298513")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
