e048eaef4c53c5ef5bb30d34480e04a9ee19a192
Robert Haken <haken@havit.cz>
2017-05-16 19:32:21 +0200

    InternalsVisibleTo

---
+++ CSharpTipsTricks/InternalsVisibleTo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// usualy placed in /Properties/AssemblyInfo.cs
+[assembly: InternalsVisibleTo("Havit.Demos.CSharpTipsTricksTests")]
+[assembly: InternalsVisibleTo("Havit.Demos.CSharpTipsTricksTests.Bllaaaah")]  // you can identify as many assemblies you want (does not have to exist)
+
+namespace Havit.Demos.CSharpTipsTricks
+{
+    internal class InternalsToTest
+	{
+		internal int Add(int value1, int value2)
+		{
+			return value1 + value2;
+		}      
+    }
+}
+
---
+++ CSharpTipsTricksTests/InternalsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Havit.Demos.CSharpTipsTricks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Demos.CSharpTipsTricksTests
+{
+	[TestClass]
+	public class InternalsTest
+	{
+		[TestMethod]
+		public void InternalsTest_Add()
+		{
+			// arrange
+			var sut = new InternalsToTest();
+
+			// act
+			var result = sut.Add(1, 2);
+
+			// assert
+			Assert.AreEqual(3, result);
+		}
+	}
+}
